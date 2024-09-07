6e6d2897afa94633dae3072dc46487d2ce942e24
Bull-Catch <kevinmorenog26@gmail.com>
2024-09-07 12:37:33 -0500

    se agrego el complemento de división y cumpleaños

--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -4,20 +4,25 @@ package ejercicios;
 import static ejercicios.SetDeTenis.evaluar;
 import java.util.Scanner;
 
-
 public class AnnosBisiestos {
-    public static String evaluar(int anno){
-        // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-        return "";
-    }
+
+    public static boolean evaluar(int anno) {
+        if (anno <= 1582) {
+            System.out.println("");
+
+        } else {
+            return anno % 4 == 0 && (anno % 100 != 0 || anno % 400 == 0);
+        }
+        return false;        
+
     
-    public static void main(String[] args) {
+    public static void main (String[] args ) {
         Scanner lector = new Scanner(System.in);
         System.out.print("Año:");
         int anno = lector.nextInt();
-        
-        String respuesta = evaluar(anno);
+
+        boolean respuesta = evaluar(anno);
         System.out.println(respuesta);
     }
-            
+
 }
